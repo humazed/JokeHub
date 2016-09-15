307e9f1fdb5a48841bd91f19e3c444d8a59494b6
huma <humazed@gmail.com>
2016-09-15 22:54:43 +0300

    add GCM

--- app/src/main/java/com/example/huma/jokehub/MainActivity.java
+++ app/src/main/java/com/example/huma/jokehub/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.huma.jokehub;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.JokeProvider;
 import com.example.huma.jokedisplay.DisplayActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EndpointsAsyncTask.OnResultReturned {
 
     private String mJoke;
 
@@ -19,13 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mJoke = JokeProvider.getJoke();
-
     }
 
     public void tellJoke(View view) {
         Toast.makeText(this, mJoke, Toast.LENGTH_SHORT).show();
+
+        EndpointsAsyncTask task = new EndpointsAsyncTask(this);
+        task.execute(new Pair<Context, String>(this, mJoke));
+
+    }
+
+    @Override
+    public void onResult(String s) {
         startActivity(new Intent(this, DisplayActivity.class)
                 .putExtra("joke", mJoke));
     }
-
 }
