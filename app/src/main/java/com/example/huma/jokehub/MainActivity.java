e995b1726bdc355213e294527ae758d87bdf53ef
huma <humazed@gmail.com>
2016-09-15 20:47:36 +0300

    add the starter code from udacity template

--- app/src/main/java/com/example/huma/jokehub/MainActivity.java
+++ app/src/main/java/com/example/huma/jokehub/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.huma.jokehub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        
     }
+
+    public void tellJoke(View view) {
+        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+    }
+
 }
